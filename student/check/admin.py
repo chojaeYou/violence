ec0b3b12b737ff3775a1daed208c6ffce3cf87a7
litmuss <sunsu2737@gmail.com>
2021-03-31 17:07:50 +0900

    export

--- student/check/admin.py
+++ student/check/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
+from import_export.admin import ExportActionModelAdmin,ImportExportMixin,ImportMixin
 from .models import Student
 # Register your models here.
 
-class StudentAdmin(admin.ModelAdmin):
+class StudentAdmin(ImportExportMixin,admin.ModelAdmin):
     list_display=('id','school','grade','name','phone','time','date','st_phone','fee_day','step')
 
 admin.site.register(Student,StudentAdmin)
